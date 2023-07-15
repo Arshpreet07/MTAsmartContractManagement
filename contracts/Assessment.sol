93b44d306e2defdb5f0a3bfbcecb3c5fe51616d2
Arshpreet07 <95047782+Arshpreet07@users.noreply.github.com>
2023-07-15 19:06:53 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,60 +1,29 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
-
-//import "hardhat/console.sol";
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
 
 contract Assessment {
     address payable public owner;
     uint256 public balance;
-
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
 
-    constructor(uint initBalance) payable {
-        owner = payable(msg.sender);
-        balance = initBalance;
+    constructor(uint initbalance) payable{
+        owner=payable(msg.sender);
+        balance= initbalance;
     }
-
+    function account() public view returns(address){
+        return owner;
+    }
     function getBalance() public view returns(uint256){
         return balance;
     }
-
-    function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
-
-        // make sure this is the owner
-        require(msg.sender == owner, "You are not the owner of this account");
-
-        // perform transaction
-        balance += _amount;
-
-        // assert transaction completed successfully
-        assert(balance == _previousBalance + _amount);
-
-        // emit the event
+    function deposit(uint256 _amount) public payable{
+        balance+=_amount;
         emit Deposit(_amount);
     }
-
-    // custom error
-    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
-
     function withdraw(uint256 _withdrawAmount) public {
-        require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
-        if (balance < _withdrawAmount) {
-            revert InsufficientBalance({
-                balance: balance,
-                withdrawAmount: _withdrawAmount
-            });
-        }
-
-        // withdraw the given amount
-        balance -= _withdrawAmount;
-
-        // assert the balance is correct
-        assert(balance == (_previousBalance - _withdrawAmount));
-
-        // emit the event
+        require(msg.sender==owner,"Only owner has access!");
+        balance-=_withdrawAmount;
         emit Withdraw(_withdrawAmount);
     }
 }
